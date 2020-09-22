dc744d6b488116ba3043b1009333838fc85e2e80
Diego serrano <diiegos89@gmail.com>
2020-09-22 08:55:27 -0300

    dark-mode

---
+++ js/app.js
@@ -0,0 +1,11 @@
+const checkbox = document.querySelector("#checkbox");
+
+checkbox.addEventListener("change", function () {
+  if (this.checked) {
+    document.body.classList.remove("is-light-mode");
+    document.body.classList.add("is-dark-mode");
+  } else {
+    document.body.classList.remove("is-dark-mode");
+    document.body.classList.add("is-light-mode");
+  }
+});
